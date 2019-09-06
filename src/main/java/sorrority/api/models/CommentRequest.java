d7ec815a0f58aa14f139ab8c1ae2c7ea5d9b5c22
Andrew Levine <andrewlevine@vagelos-ve703-06417.apn.wlan.private.upenn.edu>
2019-09-06 12:38:25 -0400

    Added the models for announcements, comments, and shoutouts

--- src/main/java/sorrority/api/models/CommentRequest.java
+++ src/main/java/sorrority/api/models/CommentRequest.java
@@ -12,13 +12,7 @@ import org.hibernate.validator.constraints.NotBlank;
 @EqualsAndHashCode
 @ToString
 @Getter
-public final class MemberRequest {
-    @NotBlank String name;
-    @NotBlank private UUID chapterUuid;
-    @NotBlank private UUID pledgeClassUuid;
-    @NotBlank private String graduatingYear;
-    @NotBlank private Boolean executive;
-    @NotBlank private String phoneNumber;
-    @NotBlank private String email;
-    @NotBlank private String birthday;
+public final class CommentRequest {
+    @NotBlank private String comment;
+    @NotBlank private UUID eventUuid;
 }
