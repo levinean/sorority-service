f4cad45dd50b14d5c234fd7399b5f6c404489bf5
Andrew Levine <andrewlevine@vagelos-ve703-06417.apn.wlan.private.upenn.edu>
2019-09-06 12:31:14 -0400

    Added models for sorrority,chapter,and pledge class

---
+++ src/main/java/sorrority/api/models/ChapterRequest.java
@@ -0,0 +1,18 @@
+package sorrority.api.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.NotBlank;
+
+@AllArgsConstructor(onConstructor = @__(@JsonCreator))
+@EqualsAndHashCode
+@ToString
+@Getter
+public final class ChapterRequest {
+    @NotBlank String name;
+    @NotBlank private UUID sorrorityUuid;
+}
---
+++ src/main/java/sorrority/api/models/SorrorityResponse.java
@@ -0,0 +1,17 @@
+package sorrority.api.models;
+
+import java.util.UUID;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.NotBlank;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@Builder
+public final class SorrorityResponse {
+    @NotBlank private UUID uuid;
+    @NotBlank private String name;
+}
